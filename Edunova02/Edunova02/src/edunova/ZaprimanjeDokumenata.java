fde63bf53a978c5853f5038063d5d8c3987f406b
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-06-14 23:27:49 +0200

    materijali s nastave edunova2

---
+++ Edunova02/Edunova02/src/edunova/ZaprimanjeDokumenata.java
@@ -0,0 +1,12 @@
+package edunova;
+
+public class ZaprimanjeDokumenata {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello");
+	}
+}
+
